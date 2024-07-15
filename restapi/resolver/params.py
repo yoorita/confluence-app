66395e062bf9fd3f5aaef5570cfe15b425358974
Rita Lykova <ritaa.lykova@gmail.com>
2024-07-15 16:07:22 +0300

    for api request params replace _ with -

--- restapi/resolver/params.py
+++ restapi/resolver/params.py
@@ -7,37 +7,5 @@ def value(val):
         return val
 
 
-def key(k):
-    match k:
-        case 'ids':
-            return 'ids'
-        case 'keys':
-            return 'keys'
-        case 'space_type':
-            return 'type'
-        case 'status':
-            return 'status'
-        case 'labels':
-            return 'labels'
-        case 'favorited_by':
-            return 'favorited-by'
-        case 'not_favorited_by':
-            return 'not-favorited-by'
-        case 'sort':
-            return 'sort'
-        case 'description_format':
-            return 'description-format'
-        case 'include_icon':
-            return 'include-icon'
-        case 'cursor':
-            return 'cursor'
-        case 'limit':
-            return 'limit'
-        case 'include_operations':
-            return 'include-operations'
-        case 'include_properties':
-            return 'include-properties'
-        case 'include_permissions':
-            return 'include-permissions'
-        case 'include_labels':
-            return 'include-labels'
+def key(k: str):
+    return k.replace("_", "-")
